cf979b1aa42f0912f624a27cd4330bf5ee275744
yeyione <jennifer.estradaavendano@gmail.com>
2025-02-21 12:20:22 -0800

    Cambios de 21-02-25

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,19 +1,11 @@
 from django.urls import path
-from . import views
+from . import views 
+
 
 urlpatterns = [
-    path("", views.index),
-    path("January", views.january),
-    path("February", views.february),
-    path("March", views.march), 
-    path("April",views.april),
-    path("May", views.may),
-    path("June", views.june),
-    path("July", views.july),
-    path("August", views.august),
-    path("September", views.september),
-    path("October", views.october),
-    path("November", views.november),
-    path("December", views.december)
+    path("", views.index ),
+    #path("<int:month>/", views.monthly_challenges_numer),#
+    path("<str:month>/", views.monthly_challenges, name = "month-challenge"),
+    
 ]
     
--- challenges/views.py
+++ challenges/views.py
@@ -1,30 +1,43 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+challenge = {
+    "january" : "Walk for at least 30 min every day",
+    "february" : "Go to the gym every day",
+    "march" : "Eat healthy every day",
+    "april" : "Sleep 8 hours every day",
+    "may" : "Be active",
+    "june" : "Stay hydrated",
+    "july" : "Dont eat to much sweet",
+    "august" : "Practice meditation",
+    "september" : "Disconnect from electronics before bedtime",
+    "october" : "Get enough sunlight",
+    "november" : "Wash your hands frequently",
+    "december" : "Spend time outside"
+}
 
 def index(request):
     return HttpResponse("Este es un mensaje desde el index")
-def january(request):
-    return HttpResponse("Walk for at least 30 min every day")
-def february(request):
-    return HttpResponse("Go to the gym every day")
-def march(request):
-    return HttpResponse("Eate healthy every day")
-def april(request):
-    return HttpResponse("Sleep 8hr every day")
-def may(request):
-    return HttpResponse("Be activated")
-def june(request):
-    return HttpResponse("Stay hydrated")
-def july(request):
-    return HttpResponse("Dont eat to much sweet")
-def august(request):
-    return HttpResponse("Practice meditation")
-def september(request):
-    return HttpResponse("Disconnect from screens before bedtime")
-def october(request):
-    return HttpResponse("Get enough sunlight")
-def november(request):
-    return HttpResponse("Wash your hands frequently")
-def december(request):
-    return HttpResponse("Spend time outdoors")
 
+#def monthly_challenges(request, month):
+    #challenge_text = challenge.get(month)
+    #if challenge_text:
+    #    return HttpResponse(challenge_text)
+   # else:
+  #      return HttpResponseNotFound("Este mes no existe")
+#def monthly_challenges_numer(request, month):
+#    month_name = dicc.get(month)
+ #   if month_name:
+  #      return monthly_challenges(request, month_name)
+   # else:
+    #    return HttpResponseNotFound("Este mes no existe")##
+
+def  monthly_challenges(request, month):
+    #try:
+        challenge_text = challenge[month]
+        return render (request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month
+        })
+    #except:
+        return HttpResponseNotFound("<h1> Este mes no existe</h1>")
